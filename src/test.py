83320df34b9f723490f484157553dbb920b604f0
Filip Gulan <filip.gulan@infinum.hr>
2018-05-24 22:44:00 +0200

    Implemented denoising.

--- src/test.py
+++ src/test.py
@@ -1,17 +1,17 @@
 import os
 import cv2
 import numpy as np
-from filters.binarization.adaptive_gauss import GaussBinarization
+from filters.noise.noise_removal import NoiseRemoval
+from filters.binarization import otsu, sauvola, adaptive_gauss
 
 from utils.helpers import load_image, debug_display_image, debug_plot_array
 
-input_image = load_image("../data/test.jpg")
+input_image = load_image("../data/testaa.jpg")
+binarizer = otsu.OtsuBinarization()
+binarized_img = binarizer.process(input_image)
+binarized_img = cv2.bitwise_not(binarized_img)
+denoiser = NoiseRemoval()
+out_image = denoiser.process(binarized_img)
 
-height, width = input_image.shape
-min_dim = min(height, width)
-block_size = int(min_dim * 0.1)
-if block_size % 2 == 0:
-    block_size += 1
-sauvola_bin = GaussBinarization(block_size=block_size, C=60)
-out_image = sauvola_bin.process(input_image)
+
 debug_display_image(out_image)
