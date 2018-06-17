b49df0e2c8cfc5126b83eb9609c6dd9cf57541e1
Filip Gulan <filip.gulan@infinum.hr>
2018-06-17 19:30:31 +0200

    Stats.

---
+++ src/ocr/wrac.sh
@@ -0,0 +1,32 @@
+wordacc ../../data/test.txt out/arial.txt stats/arial.txt
+wordacc ../../data/test.txt out/arial-bold.txt stats/arial-bold.txt
+
+wordacc ../../data/test.txt out/calibri.txt stats/calibri.txt
+wordacc ../../data/test.txt out/calibri-bold.txt stats/calibri-bold.txt
+
+wordacc ../../data/test.txt out/century-gothic.txt stats/century-gothic.txt
+wordacc ../../data/test.txt out/century-gothic-bold.txt stats/century-gothic-bold.txt
+
+wordacc ../../data/test.txt out/comic.txt stats/comic.txt
+wordacc ../../data/test.txt out/comic-bold.txt stats/comic-bold.txt
+
+wordacc ../../data/test.txt out/courier.txt stats/courier.txt
+wordacc ../../data/test.txt out/courier-bold.txt stats/courier-bold.txt
+
+wordacc ../../data/test.txt out/garamond.txt stats/garamond.txt
+wordacc ../../data/test.txt out/garamond-bold.txt stats/garamond-bold.txt
+
+wordacc ../../data/test.txt out/helvetica.txt stats/helvetica.txt
+wordacc ../../data/test.txt out/helvetica-bold.txt stats/helvetica-bold.txt
+
+wordacc ../../data/test.txt out/sans-serif.txt stats/sans-serif.txt
+wordacc ../../data/test.txt out/sans-serif-bold.txt stats/sans-serif-bold.txt
+
+wordacc ../../data/test.txt out/times.txt stats/times.txt
+wordacc ../../data/test.txt out/times-bold.txt stats/times-bold.txt
+
+wordacc ../../data/test.txt out/tnr.txt stats/tnr.txt
+wordacc ../../data/test.txt out/tnr-bold.txt stats/tnr-bold.txt
+
+wordacc ../../data/test.txt out/verdana.txt stats/verdana.txt
+wordacc ../../data/test.txt out/verdana-bold.txt stats/verdana-bold.txt
